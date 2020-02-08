47e501ac638475bb1121238e89b1fd3fd8f8e992
Matúš Sulír <matus.sulir@gmail.com>
2020-02-08 22:25:16 +0100

    Update project and make it compilable with Java 11

--- Agent/src/main/java/com/github/sulir/runtimesearch/agent/SearchAgent.java
+++ Agent/src/main/java/com/github/sulir/runtimesearch/agent/SearchAgent.java
@@ -2,7 +2,9 @@ package com.github.sulir.runtimesearch.agent;
 
 import com.github.sulir.runtimesearch.runtime.Check;
 
+import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
 import java.util.regex.Pattern;
 
 public class SearchAgent {
@@ -14,18 +16,21 @@ public class SearchAgent {
     public static void premain(String agentArgs, Instrumentation inst) {
         Pattern shouldInstrument = Pattern.compile((agentArgs == null) ? ".*" : agentArgs);
 
-        inst.addTransformer((loader, className, classBeingRedefined, protectionDomain,
-                             classfileBuffer) -> {
-            try {
-                if (className != null && shouldInstrument.matcher(className).matches()) {
-                    ClassTransformer transformer = new ClassTransformer(loader);
-                    return transformer.transform(classfileBuffer);
+        inst.addTransformer(new ClassFileTransformer() {
+            @Override
+            public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
+                    ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+                try {
+                    if (className != null && shouldInstrument.matcher(className).matches()) {
+                        ClassTransformer transformer = new ClassTransformer(loader);
+                        return transformer.transform(classfileBuffer);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
 
-            return null;
+                return null;
+            }
         });
     }
 }
