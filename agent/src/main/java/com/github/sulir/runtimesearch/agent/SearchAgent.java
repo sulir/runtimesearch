6964c67cf07f92d56d590b22d8eaf07955b85e88
Matúš Sulír <matus.sulir@gmail.com>
2021-02-04 21:56:45 +0100

    Do not instrument JDK and some agent classes by default

--- agent/src/main/java/com/github/sulir/runtimesearch/agent/SearchAgent.java
+++ agent/src/main/java/com/github/sulir/runtimesearch/agent/SearchAgent.java
@@ -5,23 +5,34 @@ import com.github.sulir.runtimesearch.runtime.Check;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class SearchAgent {
+    private static final List<String> defaultExclude = Arrays.asList(
+            "com/sun/", "java/", "javax/", "jdk/", "sun/",
+            "com/intellij/rt/", "org/jetbrains/capture/", "org/groovy/debug/", "groovyResetJarjarAsm/",
+            "org/objectweb/asm/", "com/github/sulir/runtimesearch/"
+    );
+
     static {
         Check.initialize();
         Check.runServer();
     }
 
     public static void premain(String agentArgs, Instrumentation inst) {
-        Pattern shouldInstrument = Pattern.compile((agentArgs == null) ? ".*" : agentArgs);
+        Pattern include = Pattern.compile((agentArgs == null) ? ".*" : agentArgs);
+        List<String> exclude = (agentArgs == null) ? defaultExclude : Collections.emptyList();
 
         inst.addTransformer(new ClassFileTransformer() {
             @Override
             public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
                     ProtectionDomain protectionDomain, byte[] classfileBuffer) {
                 try {
-                    if (className != null && shouldInstrument.matcher(className).matches()) {
+                    if (className != null && include.matcher(className).matches()
+                            && exclude.stream().noneMatch(className::startsWith)) {
                         ClassTransformer transformer = new ClassTransformer(className, classfileBuffer);
                         return transformer.transform();
                     }
