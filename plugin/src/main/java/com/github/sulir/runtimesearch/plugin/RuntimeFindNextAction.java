db4d3ea6383f2b82d154616ea13a26b05455beea
Matúš Sulír <matus.sulir@gmail.com>
2021-10-07 22:56:47 +0200

    Fix NPE in menu actions when no project is active

--- plugin/src/main/java/com/github/sulir/runtimesearch/plugin/RuntimeFindNextAction.java
+++ plugin/src/main/java/com/github/sulir/runtimesearch/plugin/RuntimeFindNextAction.java
@@ -2,11 +2,17 @@ package com.github.sulir.runtimesearch.plugin;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class RuntimeFindNextAction extends AnAction {
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        e.getPresentation().setEnabled(e.getProject() != null);
+    }
 
     @Override
-    public void actionPerformed(AnActionEvent event) {
-        RuntimeFindManager.getInstance(event.getProject()).findNext();
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        if (e.getProject() != null)
+            RuntimeFindManager.getInstance(e.getProject()).findNext();
     }
 }
