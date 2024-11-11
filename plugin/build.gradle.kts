4367d17e992cdb1a8866622b1cd60ad2b4c5d0e3
Matúš Sulír <matus.sulir@gmail.com>
2024-11-11 15:05:26 +0100

    Migrate to IntelliJ Platform Gradle Plugin and update IntelliJ versions

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,9 +1,17 @@
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.models.ProductRelease
 
 plugins {
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
     id("org.jetbrains.changelog") version "2.2.1"
+}
+
+repositories {
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
@@ -11,47 +19,56 @@ dependencies {
     runtimeOnly(project(":runtimesearch-agent")) {
         isTransitive = false
     }
+
+    intellijPlatform {
+        intellijIdeaCommunity("2024.2.4")
+        bundledPlugin("com.intellij.java")
+        instrumentationTools()
+    }
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_11
-val intellijVersions = listOf("2020.3.4", "2021.1.3", "2021.2.4", "2021.3.3", "2022.1.4", "2022.2.4", "2022.3.1")
+java.sourceCompatibility = JavaVersion.VERSION_17
 
-intellij {
-    version = intellijVersions.last()
-    plugins = listOf("java")
-    updateSinceUntilBuild = false
+intellijPlatform {
+    pluginConfiguration {
+        description = run {
+            val lines = rootProject.file("README.md").readLines()
+            val start = lines.indexOf("<!--plugin-desc-->")
+            val end = lines.indexOf("<!--/plugin-desc-->")
+            val description = lines.subList(start + 1, end).joinToString("\n")
+            val readme = markdownToHTML(description)
+
+            "<p>Searches the given text in all string expressions of a debugged Java SE program.<p>\n\n$readme"
+        }
+
+        changeNotes.set(provider {
+            changelog.getAll().map {
+                "<b>${it.key}</b>${changelog.renderItem(it.value.withHeader(false), Changelog.OutputType.HTML)}"
+            }.joinToString("")
+        })
+    }
+
+    pluginVerification {
+        ides {
+            select {
+                types = listOf(IntelliJPlatformType.IntellijIdeaCommunity)
+                channels = listOf(ProductRelease.Channel.RELEASE)
+                sinceBuild = "223"
+            }
+        }
+    }
+
+    publishing {
+        token = System.getenv("PUBLISH_TOKEN")
+        channels = listOf(if (version.toString().contains("snapshot")) "snapshot" else "default")
+    }
 }
 
 changelog {
-    version.set(rootProject.version.toString())
+    version = rootProject.version.toString()
     path = "${project.rootDir}/CHANGELOG.md"
     headerParserRegex = Regex("(\\d+\\.\\d+)")
     unreleasedTerm = "Unreleased"
-}
-
-tasks.patchPluginXml {
-    val firstVersion = intellijVersions[0].split(".")
-    sinceBuild = firstVersion[0].substring(2) + firstVersion[1]
-
-    pluginDescription = run {
-        val lines = rootProject.file("README.md").readLines()
-        val start = lines.indexOf("<!--plugin-desc-->")
-        val end = lines.indexOf("<!--/plugin-desc-->")
-        val description = lines.subList(start + 1, end).joinToString("\n")
-        val readme = markdownToHTML(description, "\n")
-
-        "<p>Searches the given text in all string expressions of a debugged Java SE program.<p>\n\n$readme"
-    }
-
-    changeNotes.set(provider {
-        changelog.getAll().map {
-            "<b>${it.key}</b>${changelog.renderItem(it.value.withHeader(false), Changelog.OutputType.HTML)}"
-        }.joinToString("")
-    })
-}
-
-tasks.buildSearchableOptions {
-    enabled = false
 }
 
 tasks.buildPlugin {
@@ -59,18 +76,8 @@ tasks.buildPlugin {
     destinationDirectory = project.rootProject.file("dist")
 }
 
-tasks.runPluginVerifier {
-    ideVersions = intellijVersions
-}
-
-tasks.publishPlugin {
-    token = System.getenv("PUBLISH_TOKEN")
-    channels = listOf(if (version.toString().contains("snapshot")) "snapshot" else "default")
-}
-
 tasks.runIde {
     dependsOn(":runtimesearch-agent:jar")
-    jvmArgs("--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
 }
 
 tasks.clean {
