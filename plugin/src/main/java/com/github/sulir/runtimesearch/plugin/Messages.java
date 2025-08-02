e728fe7e17ef889abed5e8a802e725e52fd9fdc6
Matúš Sulír <matus.sulir@gmail.com>
2025-08-02 17:08:54 +0200

    Move messages file to directory as recommended

--- plugin/src/main/java/com/github/sulir/runtimesearch/plugin/Messages.java
+++ plugin/src/main/java/com/github/sulir/runtimesearch/plugin/Messages.java
@@ -3,7 +3,7 @@ package com.github.sulir.runtimesearch.plugin;
 import java.util.ResourceBundle;
 
 public class Messages {
-    private static final ResourceBundle bundle = ResourceBundle.getBundle("messages");
+    private static final ResourceBundle bundle = ResourceBundle.getBundle("messages.Bundle");
 
     public static String get(String key) {
         return bundle.getString(key);
